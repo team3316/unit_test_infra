db7d55a17dadd3f998ac720471aa0cecb287a5aa
Ido Bleiberg <idoblibarge@gmail.com>
2020-05-11 23:32:24 +0300

    Added run schedualer util

--- src/test/java/frc/utils/Utils.java
+++ src/test/java/frc/utils/Utils.java
@@ -5,7 +5,23 @@ import static org.mockito.Mockito.spy;
 
 import java.lang.reflect.Field;
 
+import org.junit.After;
+import org.junit.BeforeClass;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 public class Utils {
+
+    @BeforeClass
+    public static void initMockedRobotState() {
+        MockedRobotState.getInstance();
+    }
+
+    @After
+    public void clearScheduler() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
     public static Object GetPrivate(Object base, String fieldName) {
         Field field;
         try {
@@ -75,4 +91,17 @@ public class Utils {
 
         return mock;
     }
+
+    public static void runSchedualer(int ticks) {
+        MockedRobotState.getInstance().enableRobot();
+        CommandScheduler.getInstance().enable();
+
+        for (int i = 0; i < ticks; i++) {
+            CommandScheduler.getInstance().run();
+        }
+
+        CommandScheduler.getInstance().disable();
+        MockedRobotState.getInstance().disableRobot();
+    }
+
 }
