903d3cdc5c16500087e4f31ada575d991adfc7c1
Ido Bleiberg <idoblibarge@gmail.com>
2020-05-11 23:32:11 +0300

    Added joysticks to robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.HumanIO.Joysticks;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 
@@ -33,7 +34,7 @@ public class Robot extends TimedRobot {
   public static boolean intakeRollers;
 
   /* Joysticks */
-  
+  public static Joysticks joysticks;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -43,12 +44,15 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    //m_robotContainer = new RobotContainer();
 
     arm = new Arm();
     drivetrain = new Drivetrain();
 
     intakeRollers = false;
+
+    joysticks = new Joysticks();
+    joysticks.initButtons();
 
   }
 
