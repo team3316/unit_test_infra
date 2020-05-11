99f6905a2dc79fdcdbc3f1fe2d267758b9c5d113
Ido Bleiberg <idoblibarge@gmail.com>
2020-05-11 23:34:45 +0300

    Added test comman group

--- src/test/java/frc/robot/commands/TestCommands.java
+++ src/test/java/frc/robot/commands/TestCommands.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import org.junit.After;
 import org.junit.BeforeClass;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.utils.MockedRobotState;
@@ -28,6 +29,22 @@ public abstract class TestCommands {
         if (!_cmd.isScheduled())
             _cmd.schedule();  // If cmd isn't running, push it to scheduler.
             
+        do {  // Run scheduler while command is running
+            CommandScheduler.getInstance().run();
+        } while (!_cmd.isFinished() && untilFinished);
+
+        CommandScheduler.getInstance().disable();
+        MockedRobotState.getInstance().disableRobot();
+    }
+
+    protected static void runCommandGroup(DBugCommandGroup _cmd, boolean untilFinished) {
+        MockedRobotState.getInstance().enableRobot();
+        CommandScheduler.getInstance().enable();
+
+        if (!_cmd.isScheduled()) {
+            _cmd.schedule();
+        }
+
         do {  // Run scheduler while command is running
             CommandScheduler.getInstance().run();
         } while (!_cmd.isFinished() && untilFinished);
