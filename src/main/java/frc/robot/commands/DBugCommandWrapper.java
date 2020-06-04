18ad088b38771edb313896734a0b8356fd31ce19
Tommy KA <poopmckaki@gmail.com>
2020-06-04 19:41:25 +0300

    wrapper updates

--- src/main/java/frc/robot/commands/DBugCommandWrapper.java
+++ src/main/java/frc/robot/commands/DBugCommandWrapper.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
@@ -17,30 +18,28 @@ public class DBugCommandWrapper extends DBugCommand {
    */
   public DBugCommandWrapper(CommandBase command) {
     this.command = command;
+    addRequirements(command.getRequirements().toArray(new Subsystem[0]));
   }
 
+  @Override
   public void init () {
     command.initialize();
   };
 
+  @Override
   public void execute() {
     command.execute();
   };
 
-  @Override
-  public void cancel() {
-    super.cancel();
-  }
 
   @Override
   public void end(boolean interrupted) {
-    command.end(interrupted);
     fin(interrupted);
   }
-
+  
   @Override
   protected void fin(boolean interrupted) {
-    
+    command.end(interrupted);
   }
 
   @Override
